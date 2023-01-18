a07242b9d4c283be40c3ccf13a8ba8d36772031a
Maurice Loskyll <maurice@loskyll.de>
2023-01-18 15:59:24 +0100

    Remove unneeded type specification

--- src/jolly_jumper.cpp
+++ src/jolly_jumper.cpp
@@ -6,7 +6,7 @@ bool JollyJumper::IsJollyJumper(const std::vector<int>& numbers)
 {
     return !numbers.empty() &&
            (std::adjacent_find(numbers.begin(), numbers.end(),
-                               [differences{std::vector<bool>(numbers.size(), false)}, diff{0u}](int a, int b) mutable {
+                               [differences{std::vector(numbers.size(), false)}, diff{0u}](int a, int b) mutable {
                                    differences.at(diff) = true;
                                    diff = static_cast<unsigned int>(std::abs(a - b));
                                    return !(diff > 0 && diff < differences.size() && !differences.at(diff));
