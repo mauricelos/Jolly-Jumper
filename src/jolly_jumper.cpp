a2f305224dd850257845a3d502d7098f87406273
Maurice Loskyll <maurice@loskyll.de>
2023-01-18 16:21:48 +0100

    Remove redundant check

--- src/jolly_jumper.cpp
+++ src/jolly_jumper.cpp
@@ -9,7 +9,7 @@ bool JollyJumper::IsJollyJumper(const std::vector<int>& numbers)
                                [differences{std::vector(numbers.size(), false)}, diff{0u}](int a, int b) mutable {
                                    differences.at(diff) = true;
                                    diff = static_cast<unsigned int>(std::abs(a - b));
-                                   return !(diff > 0 && diff < differences.size() && !differences.at(diff));
+                                   return !(diff < differences.size() && !differences.at(diff));
                                }) == numbers.end() ||
             std::equal(std::next(numbers.begin()), numbers.end(), numbers.begin()));
 }
