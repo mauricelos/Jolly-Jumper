b2d87e89676660453f1a6f5e8bcd5c78c31d47bf
Maurice Loskyll <maurice@loskyll.de>
2023-04-20 14:54:17 +0200

    Use std::make_unsigned_t for diff and more auto deduction

--- src/jolly_jumper.cpp
+++ src/jolly_jumper.cpp
@@ -6,9 +6,9 @@ bool JollyJumper::IsJollyJumper(const std::vector<int>& numbers)
 {
     return !numbers.empty() &&
            (std::adjacent_find(numbers.begin(), numbers.end(),
-                               [differences{std::vector<bool>(numbers.size())}, diff{0u}](int a, int b) mutable {
+                               [differences{std::vector<bool>(numbers.size())}, diff{0u}](auto a, auto b) mutable {
                                    differences.at(diff).flip();
-                                   diff = static_cast<unsigned int>(std::abs(a - b));
+                                   diff = std::make_unsigned_t<int>(std::abs(a - b));
                                    return !(diff < differences.size() && !differences.at(diff));
                                }) == numbers.end() ||
             std::equal(std::next(numbers.begin()), numbers.end(), numbers.begin()));
